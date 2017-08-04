11d598c61d3b5d0cc748b8a9088c702546424f3e
Leta He <leta.he.415@gmail.com>
2017-08-03 23:41:49 -0700

    Basic card flexbox layout displaying db data, no shuffling yet.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var hbs = require('express-handlebars');
 var mongo = require('mongodb');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/lunchgroups');
 
 var app = express();
 
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+// app.use('/lunchgroups', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
 var express = require('express');
+var mongodb = require('mongodb');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Lunch Groups' });
+/* Home page */
+router.get('/', function(req, res) {
+
+	// Get the mongo client and connect to 'employees' db
+	var MongoClient = mongodb.MongoClient;
+	var url = 'mongodb://localhost:27017/employees';
+
+	MongoClient.connect(url, function(err, db) {
+		if (err) {
+			console.log('Unable to connect to the server.', err);
+		} else {
+			var collection = db.collection('employeelist');
+			collection.find({}).toArray(function(err, result) {
+				if (err) {
+					res.send(err);
+				} else if (result[0].orderedlist.length) {
+					//Get the current ordered list of employees
+					var orderedList = result[0].orderedlist;
+					var groups = [];
+
+					//Calculate number of lunch groups
+					var numGroups = Math.floor(orderedList.length/4);
+
+					for (var i = 0; i < orderedList.length; i++) {
+						var groupIndex = i%numGroups;
+
+						if (!groups[groupIndex]) {
+							groups[groupIndex] = {'members': []};
+						}
+
+						groups[groupIndex].members.push({'person': orderedList[i]});
+					}
+
+					// lunchgroups: [ { members: [ {'person': <name>}, {'person': <name>}, ... },
+					// 				  { members: [ {'person': <name>}, {'person': <name>}, ... },
+					// 				  ... 
+					//              ]
+					res.render('index', {
+						lunchgroups: groups
+					});
+				} else {
+					res.send('No employee list found.');
+				}
+			});
+		}
+		db.close();
+	});
 });
 
 module.exports = router;
